696edbacb623b956b1256b786ba4c65d19c961b2
Oleg Prutz <olegp04728@gmail.com>
2025-11-29 10:43:25 +0000

    WebAssembly build

---
+++ out/clear-cube_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8b3f9a80130d6d5b: (a: number, b: number, c: number) => void;
+export const closure5837_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf358289184e6847d: (a: number, b: number) => void;
+export const closure5834_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb7d6051489787716: (a: number, b: number) => void;
+export const closure120014_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
